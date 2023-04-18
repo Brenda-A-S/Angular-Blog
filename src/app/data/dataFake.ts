0c8480e2ee2594f53a1596b274df7105b3931e7a
Brenda-A-S <brenda.antunes16@gmail.com>
2023-04-18 20:21:30 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"https://source.unsplash.com/random/?Design",
+        "title":"Design Title",
+        "description":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quaerat dolor cum exercitationem architecto soluta necessitatibus ducimus quia rem enim sit, et quod, officia, quae dolorem eligendi deleniti harum quis."
+    },
+    {
+        "id":"2",
+        "photo":"https://source.unsplash.com/random/?Designer",
+        "title":"Designer Title",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias hic expedita provident nobis obcaecati ab inventore illum, dignissimos quidem nam ex eum magni molestiae fuga culpa cupiditate quis asperiores atque?"
+    }
+]
