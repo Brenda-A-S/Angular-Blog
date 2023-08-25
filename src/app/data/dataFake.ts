248017eddbd5e69004b53df74e08921b67f9e256
Brenda-A-S <brenda.antunes16@gmail.com>
2023-08-25 15:59:17 -0300

    style: alteração de estilo do big-card

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,83 +1,83 @@
 export const dataFake = {
     "projects": [
-        
+
         {
-            "id":"1",
-            "photo":"https://user-images.githubusercontent.com/69852246/263121558-c435c1f4-f27c-4e60-ae8a-a95c8fac443e.jpg",
-            "title":"Projeto Pokedex",
-            "description":"Projeto de uma Pokedex com HTML, CSS e JavaScript, utilizando de recursos da PokeAPI com requisições feitas através de fetch API.",
-            "link":"https://github.com/Brenda-A-S/Pokedex-HTML-CSS-JS"
+            "id": "1",
+            "photo": "https://user-images.githubusercontent.com/69852246/263121558-c435c1f4-f27c-4e60-ae8a-a95c8fac443e.jpg",
+            "title": "Projeto Pokedex",
+            "description": "Projeto de uma Pokedex com HTML, CSS e JavaScript, utilizando de recursos da PokeAPI com requisições feitas através de fetch API.",
+            "link": "https://github.com/Brenda-A-S/Pokedex-HTML-CSS-JS"
         },
         {
-            "id":"4",
-            "photo":"https://user-images.githubusercontent.com/69852246/229861058-c60d97ec-6444-4c62-8c08-eed93c5552e2.png",
-            "title":"Projeto de Site para Cafeteria com Bootstrap",
-            "description":"Esse projeto foi desenvolvido com o Framework Bootstrap em um dos módulos da Formação CSS Web Developer da DIO. Ele possui elementos disponibilizados pelo Framework.",
-            "link":"https://github.com/Brenda-A-S/projetoSiteCafeteria/blob/main/README.md"
+            "id": "4",
+            "photo": "https://user-images.githubusercontent.com/69852246/229861058-c60d97ec-6444-4c62-8c08-eed93c5552e2.png",
+            "title": "Projeto de Site para Cafeteria com Bootstrap",
+            "description": "Esse projeto foi desenvolvido com o Framework Bootstrap em um dos módulos da Formação CSS Web Developer da DIO. Ele possui elementos disponibilizados pelo Framework.",
+            "link": "https://github.com/Brenda-A-S/projetoSiteCafeteria/blob/main/README.md"
         },
         {
-            "id":"7",
-            "photo":"https://user-images.githubusercontent.com/69852246/248926034-ea8997a4-fe39-4444-85a2-13618ce1de65.png",
-            "title":"GitFind React",
-            "description":"Projeto criado utilizando React consumindo a API do GitHub, onde buscamos um usuário e em seguida vemos um resumo sobre o mesmo, constando nome, nome de usuário, local, número de seguidores e seguidos, quantidade de repositórios públicos ou repositórios favoritados, listado com cards de resumo básico(nome e url).",
-            "link":"https://github.com/Brenda-A-S/GitFind-React"
+            "id": "7",
+            "photo": "https://user-images.githubusercontent.com/69852246/248926034-ea8997a4-fe39-4444-85a2-13618ce1de65.png",
+            "title": "GitFind React",
+            "description": "Projeto criado utilizando React consumindo a API do GitHub, onde buscamos um usuário e em seguida vemos um resumo sobre o mesmo, constando nome, nome de usuário, local, número de seguidores e seguidos, quantidade de repositórios públicos ou repositórios favoritados, listado com cards de resumo básico(nome e url).",
+            "link": "https://github.com/Brenda-A-S/GitFind-React"
         },
         {
-            "id":"8",
-            "photo":"https://user-images.githubusercontent.com/69852246/233815468-88d1a0e5-7d2e-4d9f-a35c-348e0f5bb2fa.png",
-            "title":"Clone BuzzFeed com Angular",
-            "description":"Esse projeto foi desenvolvido durante o bootcamp Potência Tech Angular Developer - Powered by iFood da DIO(Digital Innovation One) com o Framework Angular. Consiste na criação de um Quizz com utilização dos conceitos aprendidos durante o bootcamp. A temática que escolhi é: Qual a sua casa de Hogwarts?",
-            "link":"https://github.com/Brenda-A-S/BuzzfeedClone-sorting-hat-quizz"
+            "id": "8",
+            "photo": "https://user-images.githubusercontent.com/69852246/233815468-88d1a0e5-7d2e-4d9f-a35c-348e0f5bb2fa.png",
+            "title": "Clone BuzzFeed com Angular",
+            "description": "Esse projeto foi desenvolvido durante o bootcamp Potência Tech Angular Developer - Powered by iFood da DIO(Digital Innovation One) com o Framework Angular. Consiste na criação de um Quizz com utilização dos conceitos aprendidos durante o bootcamp. A temática que escolhi é: Qual a sua casa de Hogwarts?",
+            "link": "https://github.com/Brenda-A-S/BuzzfeedClone-sorting-hat-quizz"
         },
         {
-            "id":"9",
-            "photo":"https://user-images.githubusercontent.com/69852246/235329959-8976dc2c-ad1d-4789-b9ad-f8632dd0f134.gif",
-            "title":"Clone Twitter com Angular",
-            "description":"Esse projeto foi desenvolvido utilizando o framework Angular durante a iFood Dev Week da Digital Innovation OneDIO. Foram utilizadas linguagens que são as tecnologias fundamentais para a construção de páginas da Web modernas e responsivas: HTML e CSS.",
-            "link":"https://github.com/Brenda-A-S/Clone-Twitter-Angular"
+            "id": "9",
+            "photo": "https://user-images.githubusercontent.com/69852246/235329959-8976dc2c-ad1d-4789-b9ad-f8632dd0f134.gif",
+            "title": "Clone Twitter com Angular",
+            "description": "Esse projeto foi desenvolvido utilizando o framework Angular durante a iFood Dev Week da Digital Innovation OneDIO. Foram utilizadas linguagens que são as tecnologias fundamentais para a construção de páginas da Web modernas e responsivas: HTML e CSS.",
+            "link": "https://github.com/Brenda-A-S/Clone-Twitter-Angular"
         },
     ],
     "articles": [
         {
-            "id":"1",
-            "title":"Funções em JavaScript, conhecendo o Foreach",
-            "photo":"https://hermes.dio.me/articles/cover/2c41aac7-a9ff-4e1b-a2a3-f97694359061.jpg",
-            "link":"https://web.dio.me/articles/funcoes-em-javascript-conhecendo-o-foreach?back=%2Farticles&page=1&order=oldest"
+            "id": "1",
+            "title": "Funções em JavaScript, conhecendo o Foreach",
+            "photo": "https://hermes.dio.me/articles/cover/2c41aac7-a9ff-4e1b-a2a3-f97694359061.jpg",
+            "link": "https://web.dio.me/articles/funcoes-em-javascript-conhecendo-o-foreach?back=%2Farticles&page=1&order=oldest"
         },
         {
-            "id":"2",
-            "title":"Desafio 30 dias CSS",
-            "photo":"https://hermes.dio.me/articles/cover/5894627d-20c9-4798-a623-fe053819dac7.jpg",
-            "link":"https://web.dio.me/articles/desafio-30-dias-css?back=%2Farticles&page=1&order=oldest"
+            "id": "2",
+            "title": "Desafio 30 dias CSS",
+            "photo": "https://hermes.dio.me/articles/cover/5894627d-20c9-4798-a623-fe053819dac7.jpg",
+            "link": "https://web.dio.me/articles/desafio-30-dias-css?back=%2Farticles&page=1&order=oldest"
         },
         {
-            "id":"3",
-            "title":"Potência Tech Angular Developer - Powered by iFood",
-            "photo":"https://hermes.dio.me/articles/cover/457b8ac9-92b8-4864-98b5-4e4509e1dd93.jpg",
-            "link":"https://web.dio.me/articles/potencia-tech-angular-developer-powered-by-ifood?back=%2Farticles&page=1&order=oldest"
+            "id": "3",
+            "title": "Potência Tech Angular Developer - Powered by iFood",
+            "photo": "https://hermes.dio.me/articles/cover/457b8ac9-92b8-4864-98b5-4e4509e1dd93.jpg",
+            "link": "https://web.dio.me/articles/potencia-tech-angular-developer-powered-by-ifood?back=%2Farticles&page=1&order=oldest"
         },
         {
-            "id":"4",
-            "title":"Clone do BuzzFeed",
-            "photo":"https://hermes.dio.me/articles/cover/0f68e49c-2fe1-43b5-8f3e-ad4edc77e410.png",
-            "link":"https://web.dio.me/articles/clone-do-buzzfeed?back=%2Farticles&page=1&order=oldest"
+            "id": "4",
+            "title": "Clone do BuzzFeed",
+            "photo": "https://hermes.dio.me/articles/cover/0f68e49c-2fe1-43b5-8f3e-ad4edc77e410.png",
+            "link": "https://web.dio.me/articles/clone-do-buzzfeed?back=%2Farticles&page=1&order=oldest"
         },
     ],
     "post": [
         {
-            "id":"1",
-            "title":"Lista de links para te ajudar no desenvolvimento e aprendizado",
-            "link":"https://www.linkedin.com/feed/update/urn:li:activity:7098009850088947712/"
+            "id": "1",
+            "title": "Lista de links para te ajudar no desenvolvimento e aprendizado",
+            "link": "https://www.linkedin.com/feed/update/urn:li:activity:7098009850088947712/"
         },
         {
-            "id":"2",
-            "title":"Indicação de site para escolher paleta de cores: Pigment Shapefactory",
-            "link":"https://www.linkedin.com/posts/brenda-antunes-silva_design-uidesign-activity-7078427675437993984--UZ_?utm_source=share&utm_medium=member_desktop"
+            "id": "2",
+            "title": "Indicação de site para escolher paleta de cores: Pigment Shapefactory",
+            "link": "https://www.linkedin.com/posts/brenda-antunes-silva_design-uidesign-activity-7078427675437993984--UZ_?utm_source=share&utm_medium=member_desktop"
         },
         {
-            "id":"3",
-            "title":"Como eu gravo meus vídeos? Mostrando qual Software eu uso para gravação",
-            "link":"https://www.linkedin.com/posts/brenda-antunes-silva_github-activity-7094806365071634432-c842?utm_source=share&utm_medium=member_desktop"
+            "id": "3",
+            "title": "Como eu gravo meus vídeos? Mostrando qual Software eu uso para gravação",
+            "link": "https://www.linkedin.com/posts/brenda-antunes-silva_github-activity-7094806365071634432-c842?utm_source=share&utm_medium=member_desktop"
         },
-    ],    
+    ],
 }
