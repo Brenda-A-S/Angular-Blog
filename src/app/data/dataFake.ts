df084ca9055442d78db880c80067e828dcf41c42
Brenda-A-S <brenda.antunes16@gmail.com>
2023-08-25 14:29:26 -0300

    fix: alteração em links, alteração em tags e estilo do header

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -9,39 +9,11 @@ export const dataFake = {
             "link":"https://github.com/Brenda-A-S/Pokedex-HTML-CSS-JS"
         },
         {
-            "id":"2",
-            "photo":"https://user-images.githubusercontent.com/69852246/263068495-c4ae1e3b-9425-4fd6-a3d3-d30e8d859e1f.jpg",
-            "title":"Nails Beauty",
-            "description":"Desenvolvi e projetei um site exclusivo para serviços de manicure, cuidadosamente construído com HTML, CSS e JavaScript. Este projeto não apenas representa minha paixão pela criação web, mas também reflete meu compromisso com a excelência em design e desenvolvimento.",
-            "link":"https://github.com/Brenda-A-S/Nails-Beauty-HTML-CSS-JS"
-        },
-        {
-            "id":"3",
-            "photo":"https://user-images.githubusercontent.com/69852246/235327943-83cb1586-7130-49f4-bf87-3d633e0515fe.png",
-            "title":"Clone Orkut Angular",
-            "description":"Esse projeto foi desenvolvido utilizando o framework Angular, criei uma página de login e uma página de perfil com um exemplo. A página de perfil foi desenvolvida totalmente do ZERO por mim, baseada em algumas imagens inspirações, que você pode conferir no repositório.",
-            "link":"https://github.com/Brenda-A-S/Clone-Orkut-Angular"
-        },
-        {
             "id":"4",
             "photo":"https://user-images.githubusercontent.com/69852246/229861058-c60d97ec-6444-4c62-8c08-eed93c5552e2.png",
             "title":"Projeto de Site para Cafeteria com Bootstrap",
             "description":"Esse projeto foi desenvolvido com o Framework Bootstrap em um dos módulos da Formação CSS Web Developer da DIO. Ele possui elementos disponibilizados pelo Framework.",
             "link":"https://github.com/Brenda-A-S/projetoSiteCafeteria/blob/main/README.md"
-        },
-        {
-            "id":"5",
-            "photo":"https://user-images.githubusercontent.com/69852246/230966407-edde67bc-d3ca-47e0-88af-c7aeef215c2e.png",
-            "title":"Desafio criação de Currículo Online",
-            "description":"Este projeto foi desenvolvido para o Bootcamp Potência Tech Angular Developer - Powered by iFood da Digital Innovation One (DIO).",
-            "link":"https://github.com/Brenda-A-S/curriculo"
-        },
-        {
-            "id":"6",
-            "photo":"https://user-images.githubusercontent.com/69852246/257676008-4b115a18-bae8-4840-b694-0fa796e12884.jpg",
-            "title":"Projeto Treina Fofo",
-            "description":"Projeto criado com React, HTML5, JavaScript e SCSS para aprimorar os conhecimentos nessas tecnologias. O desafio foi recriar o design concebido pelo Futuro Dev - John, buscando aprimorar minhas habilidades e explorar novas possibilidades criativas.",
-            "link":"https://github.com/Brenda-A-S/Treina-Fofo"
         },
         {
             "id":"7",
@@ -63,13 +35,6 @@ export const dataFake = {
             "title":"Clone Twitter com Angular",
             "description":"Esse projeto foi desenvolvido utilizando o framework Angular durante a iFood Dev Week da Digital Innovation OneDIO. Foram utilizadas linguagens que são as tecnologias fundamentais para a construção de páginas da Web modernas e responsivas: HTML e CSS.",
             "link":"https://github.com/Brenda-A-S/Clone-Twitter-Angular"
-        },
-        {
-            "id":"10",
-            "photo":"https://user-images.githubusercontent.com/69852246/260861801-68a14449-d5a7-4c44-9799-475ef6d07d26.jpg",
-            "title":"QR code component",
-            "description":"Essa é uma solução para o QR code component challenge on Frontend Mentor. Frontend Mentor challenges te ajuda a melhorar suas habilidades de codificação construindo projetos realistas.",
-            "link":"https://github.com/Brenda-A-S/QR-Code-Component"
         },
     ],
     "articles": [
